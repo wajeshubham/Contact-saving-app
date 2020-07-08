d45a06f0123606af475f17d5bee771be299da084
shubham <waje.shubham111@gmail.com>
2020-07-08 20:58:17 +0530

    added config comment

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,12 +1,14 @@
 export var firebaseConfig = {
-  apiKey: "AIzaSyBSbubWtDYHCSSMUp5cENTtQQvQWqogMb0",
-  authDomain: "github-mini.firebaseapp.com",
-  databaseURL: "https://github-mini.firebaseio.com",
-  projectId: "github-mini",
-  storageBucket: "github-mini.appspot.com",
-  messagingSenderId: "240564373921",
-  appId: "1:240564373921:web:83de52cf96b6abd6a363e0",
-  measurementId: "G-C4NPSW2B9L",
+  // add your firebase project configuration here
+
+  apiKey: "api_key",
+  authDomain: "abc.firebaseapp.com",
+  databaseURL: "https://abc.firebaseio.com",
+  projectId: "your project name",
+  storageBucket: "abc.appspot.com",
+  messagingSenderId: "*****",
+  appId: "****",
+  measurementId: "****",
 };
 
 //image configuration
